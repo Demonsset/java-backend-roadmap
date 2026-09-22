c4689055345bac009b405248fa5887176cd20a28
Demonsset <hazemmohamedkenawy@gmail.com>
2026-09-22 23:07:50 +0300

    ehem

--- src/Lecture02_ControlFlow.java
+++ src/Lecture02_ControlFlow.java
@@ -1,13 +1,6 @@
 public class Lecture02_ControlFlow {
-
-    public static void main(String[] args) {
-        int month = 5;
-        String season = switch (month) {
-            case 1, 2, 3 -> "spring";
-            case 4, 5, 6 -> "summer";
-            case 7, 8, 9 -> "fall";
-            case 10, 11, 12 -> "winter";
-            default -> "else";
+    public static void main(String []args)
+    {
 
     }
 
